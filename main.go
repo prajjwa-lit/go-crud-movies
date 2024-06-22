71eea3c1f16ad2a5db4ad67a5bc58415dbde5d4d
prajjwa-lit <prajjwal026@gmail.com>
2024-06-22 11:27:06 +0530

    adds functions

---
+++ main.go
@@ -0,0 +1,83 @@
+// Description: This is a simple REST API that uses the gorilla/mux package to create a simple REST API that can perform CRUD operations on a list of movies. The API has the following endpoints:
+// 1. GET /movies - Get all movies
+// 2. GET /movies/{id} - Get a single movie
+// 3. POST /movies - Create a new movie
+// 4. PUT /movies/{id} - Update a movie
+// 5. DELETE /movies/{id} - Delete a movie
+// The API uses an in-memory slice to store the movies. The API is run on port 8000.
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"std/math/rand"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header.Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func main() {
+	r := mux.NewRouter()
+	movies = append(movies, Movie{ID: "1", Isbn: "448743", Title: "Kaptan Amrika", Director: &Director{Firstname: "Nigga", Lastname: "Delorean"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "069420", Title: "40 year old virgin", Director: &Director{Firstname: "Patrakar", Lastname: "Popatlal"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	lof.Fatal(http.ListenAndServe(":8000", r))
+
+}
